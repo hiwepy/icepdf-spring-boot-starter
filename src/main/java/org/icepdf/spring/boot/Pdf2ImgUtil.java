880e5aefa82f8e09e14ec2b9dd4c4c8f44d36666
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:35:09 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/icepdf/spring/boot/Pdf2ImgUtil.java
+++ src/main/java/org/icepdf/spring/boot/Pdf2ImgUtil.java
@@ -17,7 +17,7 @@ package org.icepdf.spring.boot;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 
 import java.awt.image.BufferedImage;
